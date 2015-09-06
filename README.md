eacbad1130bb6e510c0f6166c31db6961bf7580f
Flavio Poletti <flavio@polettix.it>
2015-09-06 03:39:16 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,28 @@
+# NAME
+
+gosh - Gamification Of Shared Housekeeping
+
+# SYNOPSIS
+
+    use Gosh;
+
+# DESCRIPTION
+
+Gosh is an attempt to gamify shared tasks, like housekeeping duties as
+normally carried on by people living in the same house. Nothing prevents
+you from using it for other purposes though!
+
+# AUTHOR
+
+Flavio Poletti <polettix@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2015 Flavio Poletti
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the Artistic License 2.0.
+
+# SEE ALSO
---
+++ lib/Gosh.pm
@@ -0,0 +1,41 @@
+package Gosh;
+
+use strict;
+use 5.018_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+gosh - Gamification Of Shared Housekeeping
+
+=head1 SYNOPSIS
+
+  use Gosh;
+
+=head1 DESCRIPTION
+
+Gosh is an attempt to gamify shared tasks, like housekeeping duties as
+normally carried on by people living in the same house. Nothing prevents
+you from using it for other purposes though!
+
+=head1 AUTHOR
+
+Flavio Poletti E<lt>polettix@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015 Flavio Poletti
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the Artistic License 2.0.
+
+=head1 SEE ALSO
+
+=cut
